dade440004c3be7b57fee41721e57cd42c577866
SSQ-Storm <Denislarioshin2004@gmail.com>
2025-02-05 12:20:12 +0500

    Step 17 Editing Project directories

--- src/index.js
+++ src/index.js
@@ -1,31 +1,7 @@
 import React from "react";
 import ReactDOM from 'react-dom/client';
-import AppHeader from './component/app-header';
-import SearchPanel from './component/search-panel';
-import TodoList from './component/todo-list';
-import ItemStatusFilter from './component/item-status-filter';
-import './component/index.css';
 
-const App = () => {
+import App from './components/app';
 
-    const todoData = [
-    { label: 'Drink Coffee', important: false,  id: 1},
-    { label: 'React Application', important: true, id: 2 },
-    { label: 'Make notes from your stud', important: false, id: 3 },
-    { label: 'Do not drink alchohol', important: false, id: 4 },
-];
-
-    return (
-        <div className="todo-app">
-            <AppHeader toDo={1} done={3} />
-            <div className="top-panel d-flex">
-                <SearchPanel />
-                <ItemStatusFilter />
-            </div>
-
-            <TodoList todos={todoData} />
-        </div>
-    );
-};
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(<App/>);
