16fe5c34c5dc55f1801ef8d396e272ca0ac46a8a
SSQ-Storm <Denislarioshin2004@gmail.com>
2025-02-19 10:01:46 +0500

    Step 33 Controlled component

--- src/components/item-add-form/item-add-form.js
+++ src/components/item-add-form/item-add-form.js
@@ -17,6 +17,9 @@ export default class ItemAddForm extends  Component {
  onSubmit = (e) => {
      e.preventDefault();
     this.props.onItemAdded(this.state.label);
+    this.setState({
+        label: ''
+    });
  };
     render() {
         return (
@@ -26,7 +29,8 @@ export default class ItemAddForm extends  Component {
                 <input type={"text"}
                         className={"form-control"}
                         onChange = {this.onLabelChange}
-                        placeholder={"What need to be done"} />
+                        placeholder={"What need to be done"} 
+                        value={this.state.label} />
                 <button
                     className={ "btn btn-outline-secondary" } >
                     Add Item
